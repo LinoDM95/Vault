2fce84bf11b4d53baa47ccb361aa9cde6e0b1c97
LinoDM95 <linodemarco95@gmail.com>
2025-03-16 22:14:01 +0100

    sync

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,25 +1,12 @@
 // am Anfang Ihres Anwendungseinstiegspunkts
-import 'vite/modulepreload-polyfill'
+import "vite/modulepreload-polyfill";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service_worker.js')
-      .then(registration => {
-        console.log('Service Worker registration with scope:', registration.scope);
-      })
-      .catch(error => {
-        console.error('Service Worker registration failed:', error);
-      });
-  });
-}
-
-
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 );
