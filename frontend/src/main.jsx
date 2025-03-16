fddc3382d2e3b520f4fe35cb5a90b48ac84da29a
LinoDM95 <linodemarco95@gmail.com>
2025-03-16 21:32:19 +0100

    added Service-Worker.js

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,19 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service_worker.js')
+      .then(registration => {
+        console.log('Service Worker registration with scope:', registration.scope);
+      })
+      .catch(error => {
+        console.error('Service Worker registration failed:', error);
+      });
+  });
+}
+
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
